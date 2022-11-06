4e23c0c0b5087de3f19d02f7933a678a130e40c6
Mahmoud Bahaa <mahmoud.mohamed.bahaa@gmail.com>
2022-11-05 19:41:56 -0700

    Remove commented code

--- streamseed/src/components/Livepeer.js
+++ streamseed/src/components/Livepeer.js
@@ -1,21 +1,6 @@
 import { Player } from '@livepeer/react';
 import * as React from 'react';
 
-/*
-import Image from 'next/image';
-const PosterImage = () => {
-  return (
-    <Image
-      src={poster}
-      layout="fill"
-      objectFit="cover"
-      priority
-      placeholder="blur"
-    />
-  );
-};
- */
-
 const playbackId = '76c2of8uysti6e23';
  
 export function Livepeer() {
