82749237414f344ccdb94e1aa47424eaabfe2875
dRadiant <103012174+dRadiant@users.noreply.github.com>
2022-11-05 17:19:50 -0700

    Start of UI

--- streamseed/src/App.js
+++ streamseed/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Livepeer } from './Livepeer';
+
+import { Box, Heading } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
+
+import { Livepeer } from './components/Livepeer';
 
 import {
   LivepeerConfig,
@@ -21,14 +27,49 @@ const livepeerTheme = {
     display: 'Inter',
   },
 };
- 
+
 function App() {
   return (
-    <LivepeerConfig client={client} theme={livepeerTheme}>
-      <Livepeer />
-    </LivepeerConfig>
+    <div className="App">
+      <Grid templateAreas={`"header header" "player chatbox" "description description" "footer footer"`} gridTemplateRows={'7vh 1fr 10vh 5vh'} gridTemplateColumns={'75vw 1fr'} h='100vh' gap='0' color='blackAlpha.700' fontWeight='bold'>
+        <GridItem pl='2' /*bg='orange.300'*/ area={'header'} className="Header">
+          <Flex>
+            <Box p="1vh">
+              <Center>StreamSeed</Center>
+            </Box>
+            <Spacer />
+            <Box p="1vh">
+              <Center>
+                <Box className="ConnectWallet">Connect Wallet</Box>
+              </Center>
+            </Box>
+          </Flex>
+        </GridItem>
+        <GridItem pl='0' /*bg='pink.300'*/ area={'player'} className="Player">
+          <Center w="100%" h="100%">
+            <Box className="PlayerContainer">
+              <LivepeerConfig client={client} theme={livepeerTheme}>
+                <Livepeer />
+              </LivepeerConfig>
+            </Box>
+          </Center>
+        </GridItem>
+        <GridItem pl='2' area={'chatbox'} className="Chatbox">
+          <Center w="100%" h="100%">
+            <Box className="ChatboxContainer">
+                <Center w="100%" h="100%">Chatbox</Center>
+            </Box>
+          </Center>
+        </GridItem>
+        <GridItem pl='2' /*bg='yellow.300'*/ area={'description'} className="Description">
+          Description
+        </GridItem>
+        <GridItem pl='2' /*bg='blue.300'*/ area={'footer'} className="Footer">
+          
+        </GridItem>
+    </Grid>
+    </div>
   );
 }
-
 
 export default App;
---
+++ streamseed/src/components/Livepeer.js
@@ -0,0 +1,34 @@
+import { Player } from '@livepeer/react';
+import * as React from 'react';
+
+/*
+import Image from 'next/image';
+const PosterImage = () => {
+  return (
+    <Image
+      src={poster}
+      layout="fill"
+      objectFit="cover"
+      priority
+      placeholder="blur"
+    />
+  );
+};
+ */
+
+const playbackId =
+  'bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe';
+ 
+export function Livepeer() {
+  return (
+    <Player
+      title="Waterfalls"
+      playbackId={playbackId}
+      loop
+      autoPlay
+      showTitle={false}
+      muted
+      poster={<div />}
+    />
+  );
+}
