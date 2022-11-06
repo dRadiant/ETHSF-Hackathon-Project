d5a12656b9b78822cba35f582fdc5ae2ac3a78b0
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-11-05 20:40:12 -0700

    rainbowkit

--- streamseed/src/App.js
+++ streamseed/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Box, Button, Heading, VStack } from '@chakra-ui/react'
-import { Center } from '@chakra-ui/react'
-import { Grid, GridItem } from '@chakra-ui/react'
-import { Flex, Spacer } from '@chakra-ui/react'
+import { Box, Button, Heading, VStack } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { Flex, Spacer } from '@chakra-ui/react';
 
-import { LockIcon } from '@chakra-ui/icons'
+import { LockIcon } from '@chakra-ui/icons';
 
 import { Livepeer } from './components/Livepeer';
 
@@ -16,11 +16,12 @@ import {
   studioProvider,
 } from '@livepeer/react';
 import React from 'react';
- 
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 const client = createReactClient({
   provider: studioProvider({ apiKey: '098c9b05-ebd9-4a18-8477-4ceade2a79b7' }),
 });
- 
+
 const livepeerTheme = {
   colors: {
     accent: 'rgb(0, 145, 255)',
@@ -34,58 +35,76 @@ const livepeerTheme = {
 // Unlock Protocol Mumbai - https://app.unlock-protocol.com/locks/lock?address=0x812b7b78ee707e1bcd32e0622888ff7d968fb2f2&network=80001
 
 class App extends React.Component {
-  state = {
-    
-  }
+  state = {};
 
   render() {
     return (
-      <div className="App">
-        <Grid templateAreas={`"header header" "player chatbox" "description description" "footer footer"`} gridTemplateRows={'7vh 1fr 10vh 5vh'} gridTemplateColumns={'75vw 1fr'} h='100vh' gap='0' color='blackAlpha.700' fontWeight='bold'>
-          <GridItem pl='2' /*bg='orange.300'*/ area={'header'} className="Header">
+      <div className='App'>
+        <Grid
+          templateAreas={`"header header" "player chatbox" "description description" "footer footer"`}
+          gridTemplateRows={'7vh 1fr 10vh 5vh'}
+          gridTemplateColumns={'75vw 1fr'}
+          h='100vh'
+          gap='0'
+          color='blackAlpha.700'
+          fontWeight='bold'
+        >
+          <GridItem
+            pl='2'
+            /*bg='orange.300'*/ area={'header'}
+            className='Header'
+          >
             <Flex>
-              <Box p="1vh">
+              <Box p='1vh'>
                 <Center>StreamSeed</Center>
               </Box>
               <Spacer />
-              <Box p="1vh">
+              <Box p='1vh'>
                 <Center>
-                  <Box className="ConnectWallet">Connect Wallet</Box>
+                  <ConnectButton />
                 </Center>
               </Box>
             </Flex>
           </GridItem>
-          <GridItem pl='0' /*bg='pink.300'*/ area={'player'} className="Player">
-            <Center w="100%" h="100%">
-              <Box className="PlayerContainer">
+          <GridItem pl='0' /*bg='pink.300'*/ area={'player'} className='Player'>
+            <Center w='100%' h='100%'>
+              <Box className='PlayerContainer'>
                 <LivepeerConfig client={client} theme={livepeerTheme}>
                   <Livepeer />
                 </LivepeerConfig>
               </Box>
             </Center>
           </GridItem>
-          <GridItem pl='2' area={'chatbox'} className="Chatbox">
-            <Center w="100%" h="100%">
-              <Box className="ChatboxContainer">
-                  <Center w="100%" h="100%">
-                    <VStack spacing="10">
-                      <LockIcon boxSize="150" />
-                      <Heading size="md" textAlign="center">
+          <GridItem pl='2' area={'chatbox'} className='Chatbox'>
+            <Center w='100%' h='100%'>
+              <Box className='ChatboxContainer'>
+                <Center w='100%' h='100%'>
+                  <VStack spacing='10'>
+                    <LockIcon boxSize='150' />
+                    <Heading size='md' textAlign='center'>
                       You must have a membership token to view and chat
-                      </Heading>
-                      <Button className="JoinButton" size="lg">Join</Button>
-                    </VStack>                    
-                  </Center>
+                    </Heading>
+                    <Button className='JoinButton' size='lg'>
+                      Join
+                    </Button>
+                  </VStack>
+                </Center>
               </Box>
             </Center>
           </GridItem>
-          <GridItem pl='2' /*bg='yellow.300'*/ area={'description'} className="Description">
+          <GridItem
+            pl='2'
+            /*bg='yellow.300'*/ area={'description'}
+            className='Description'
+          >
             Description
           </GridItem>
-          <GridItem pl='2' /*bg='blue.300'*/ area={'footer'} className="Footer">
-            
-          </GridItem>
-      </Grid>
+          <GridItem
+            pl='2'
+            /*bg='blue.300'*/ area={'footer'}
+            className='Footer'
+          ></GridItem>
+        </Grid>
       </div>
     );
   }
--- streamseed/src/index.js
+++ streamseed/src/index.js
@@ -4,15 +4,41 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode> 
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>
+      <React.StrictMode>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </React.StrictMode>
+    </RainbowKitProvider>
+  </WagmiConfig>
 );
 
 // If you want to start measuring performance in your app, pass a function
